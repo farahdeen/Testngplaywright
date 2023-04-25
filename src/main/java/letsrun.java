311dfeb0ad6bc9cfefcbaf5dfc243587b56de190
farahdeen <deenmohamed05@outlook.com>
2023-04-25 16:57:53 +0100

    gd

--- src/main/java/letsrun.java
+++ src/main/java/letsrun.java
@@ -15,12 +15,16 @@ public class letsrun {
         Page page = browser.newPage();
         page.navigate("https://demoqa.com/frames");
 
+
+
         //https://www.youtube.com/watch?v=WZbAGjFwN-o&list=PLFGoYjJG_fqrjcgRUcc2ubbZGtbRcC6W8&index=11
         //used xpath and css selector to go into frames
         String hg =  page.frameLocator("//*[@id=\"frame2\"]").locator("h1").textContent();
         System.out.println(hg);
         String hgx =  page.frameLocator("//*[@id=\"frame1\"]").locator("h1").textContent();
         System.out.println(hgx);
+
+
 
         browser.close();
         playwright.close();
